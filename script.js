105495fd5cc764a8b2bc9fffb3a61473f8c2e2de
Evan <evanacoates@gmail.com>
2023-03-05 21:35:00 -0500

    HTML and variables set up

---
+++ script.js
@@ -0,0 +1,53 @@
+let start = document.getElementById('startBtn');
+let quiz = document.getElementById('quiz');
+let question = document.getElementById('question');
+let timer = document.getElementById('timer');
+let counter = document.getElementById('counter');
+let answerA = document.getElementById('A');
+let answerB = document.getElementById('B');
+let answerC = document.getElementById('C');
+let answerD = document.getElementById('D');
+let quizProgress = document.getElementById('quizProgress')
+let quizScore = document.getElementById('quizScore')
+
+let questions = [{
+    question : "",
+    answerA : "",
+    answerB : "",
+    answerC : "",
+    answerD : "",
+    correct : "answerB"
+}
+{
+    question : "",
+    answerA : "",
+    answerB : "",
+    answerC : "",
+    answerD : "",
+    correct : "answerC"
+}
+{
+    question : "",
+    answerA : "",
+    answerB : "",
+    answerC : "",
+    answerD : "",
+    correct : "answerB"
+}
+{
+    question : "",
+    answerA : "",
+    answerB : "",
+    answerC : "",
+    answerD : "",
+    correct : "answerA"
+}
+{
+    question : "",
+    answerA : "",
+    answerB : "",
+    answerC : "",
+    answerD : "",
+    correct : "answerB"
+}]
+
